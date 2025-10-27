7f9b5a1016fb203b8f883317d33cd512a5beb310
Talat30 <talathatifa@gmail.com>
2025-10-27 21:34:21 +0530

    Initial commit - Drowsiness detection web app

--- Drowsiness_detector-main/app.py
+++ Drowsiness_detector-main/app.py
@@ -73,5 +73,13 @@ def status():
     return jsonify(status_data)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
